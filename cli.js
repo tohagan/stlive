69c9454bccf3b795657fafdeb972c5eaa6183cc2
Tony OHagan <tony@ohagan.name>
2014-08-09 13:06:14 +1000

    Make CLI flag options explicitly boolean. renamed options.rmReadmes => options.readme === false

--- cli.js
+++ cli.js
@@ -5,7 +5,10 @@ try {
         dirname: __dirname, 
         clone: true,        // Clones default.config to '~/.stlive.config' 
         ancestors: true,    // Search curr and ancestor dirs for '.stlive.config' files
-        cmdTree: require('./lib')
+        cmdTree: require('./lib'),
+        cli: {
+            boolean: [ 'version', 'run', 'autoreload', 'localtunnel', 'readme' ]
+        }
     });
 } catch (ex) {
     console.error(ex);

6a731454340dcdd3dd6de72b66bfa604b0c8addf
Tony OHagan <tony@ohagan.name>
2014-08-21 15:58:03 +1000

    Added legacy file fix to rename ~/.stlive.config files to ~/.stlive.json

--- cli.js
+++ cli.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
 try {
+    // rename ~/.stlive.config => ~/.stlive.json
+    require('./lib/legacy')();
+
     require('cli-config').run({
-        dirname: __dirname, 
-        clone: true,        // Clones .stlive.json to '~/.stlive.json' 
+        dirname: __dirname,
+        clone: true,        // Clones .stlive.json to '~/.stlive.json'
         ancestors: true,    // Search curr and ancestor dirs for '.stlive.json' files
         cmdTree: require('./lib')
     });
